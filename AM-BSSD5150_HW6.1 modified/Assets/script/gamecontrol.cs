69e22cf9d2cbd1897a35b90b6379a1e3093523a9
Amgad Morsy <42896629+amgadkamal@users.noreply.github.com>
2020-02-20 11:56:27 -0700

    hw6.1 --

---
+++ AM-BSSD5150_HW6.1 modified/Assets/script/gamecontrol.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamecontrol : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    Animator animator;
+    int state;
+    int initialState;
+    public static int control = 0;
+
+    // Start is called before the first frame update
+
+    private bool mousee;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        state = animator.GetInteger("CurrentState");
+
+    }
+
+
+    IEnumerator ColorShift()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        state = ++state % 3;
+        animator.SetInteger("CurrentState", state);
+        StopCoroutine("ColorShift");
+        StartCoroutine("ColorShift");
+
+    }
+
+
+
+    public void OnMouseDown()
+    {
+        mousee = true;
+
+    }
+
+    public void OnMouseUp()
+    {
+        mousee = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (mousee)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+
+    }
+
+    public void animation()
+    {
+
+        StartCoroutine("ColorShift");
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.transform.SetParent(this.gameObject.transform);
+        if (this.gameObject.transform.childCount == 8)
+        {
+
+            mousee = false;
+            state = 2;
+            state = animator.GetInteger("CurrentState");
+            animation();
+
+        }
+
+    }
+   
+    public void SetState(int value)
+    {
+        StopCoroutine("ColorShift");
+        initialState = value;
+        state = value;
+        animator.SetInteger("CurrentState", state);
+    }
+
+    public void ResetAndPause()
+    {
+        StopCoroutine("ColorShift");
+        state = 0;
+        animator.SetInteger("CurrentState", state);
+    }
+
+
+}
+
